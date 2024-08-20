c1216e6625010b79f40e36c8d4551fa6b135cb29
Sainathhiwale <sainathhiwale2@gmail.com>
2024-08-20 17:29:01 +0530

    count example is implemented without viewmodel and see screen rotation time value get Destroy.

--- app/src/main/java/com/examen/advancedandroidbootcamp_2024/MainActivity.kt
+++ app/src/main/java/com/examen/advancedandroidbootcamp_2024/MainActivity.kt
@@ -5,51 +5,30 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.examen.advancedandroidbootcamp_2024.databinding.ActivityMainBinding
-import com.examen.advancedandroidbootcamp_2024.model.User
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mainBinding: ActivityMainBinding
-
+    private  var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainBinding.textView.text =count.toString()
+        mainBinding.buttonCount.setOnClickListener(this)
 
-        mainBinding.buttonSubmit.setOnClickListener(this)
-        mainBinding.buttonStart.setOnClickListener(this)
-
-        val user = getUserData()
-        mainBinding.user = getUserData()
     }
-
+     onDe
     override fun onClick(v: View?) {
         when(v?.id){
-            R.id.button_submit ->{
-                getTextDisplay()
-            }
-            R.id.button_start ->{
-                startProgressBar();
+            R.id.button_count ->{
+                getCountDisplay()
             }
         }
     }
 
-    private fun startProgressBar() {
-        if (mainBinding.progressCircular.visibility == View.INVISIBLE){
-            mainBinding.progressCircular.visibility = View.VISIBLE
-            mainBinding.buttonStart.text = "Stop"
-        }else{
-            mainBinding.progressCircular.visibility = View.INVISIBLE
-            mainBinding.buttonStart.text = "Start"
-        }
-
+    fun getCountDisplay(){
+        count ++
+        mainBinding.textView.text = count.toString()
     }
 
-    private fun getTextDisplay() {
-        val text = mainBinding.editText.text.toString()
-        mainBinding.textView.text = text
-    }
 
-    // binding object data to xml file directly
-    fun getUserData():User{
-        return User("Sainath",23,"Pune","Sainath Hiwale")
-    }
 }
--- app/src/main/java/com/examen/advancedandroidbootcamp_2024/model/User.kt
+++
@@ -1,3 +0,0 @@
-package com.examen.advancedandroidbootcamp_2024.model
-
-data class User(val name:String, val age:Int, val address:String, val empName:String)
