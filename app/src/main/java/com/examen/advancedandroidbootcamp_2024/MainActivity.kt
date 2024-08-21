d2a4ee29492764bf0c465adbf70acd50fa9fc2e1
Sainathhiwale <sainathhiwale2@gmail.com>
2024-08-21 21:36:23 +0530

    viewmodel with mutabalelivedata binding

--- app/src/main/java/com/examen/advancedandroidbootcamp_2024/MainActivity.kt
+++ app/src/main/java/com/examen/advancedandroidbootcamp_2024/MainActivity.kt
@@ -8,33 +8,22 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.examen.advancedandroidbootcamp_2024.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
-    private lateinit var viewModelFactory: MainActivityViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModelFactory = MainActivityViewModelFactory(125)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
-        mainBinding.insertButton.setOnClickListener(this)
-        viewModel.totalData.observe(this, Observer {
-            mainBinding.resultTextView.text = it.toString()
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        mainBinding.myViewModel = viewModel
+        viewModel.count.observe(this, Observer {
+            mainBinding.countText.text = it.toString()
         })
-
     }
 
-    override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.insert_button ->{
-                getCountDisplay()
-            }
-        }
-    }
 
-    private fun getCountDisplay() {
-        viewModel.setTotal(mainBinding.inputEditText.text.toString().toInt())
-    }
+
+
 
 
 }
